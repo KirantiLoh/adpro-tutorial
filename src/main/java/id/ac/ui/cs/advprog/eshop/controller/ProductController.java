5c99af38feb675116334300ca7923b089533634d
Maurice Yang <mauriceyangmy28@gmail.com>
2024-02-05 15:23:08 +0700

    feat: Adding deletion feature

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -3,10 +3,13 @@ package id.ac.ui.cs.advprog.eshop.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import id.ac.ui.cs.advprog.eshop.model.Product;
@@ -37,6 +40,33 @@ public class ProductController {
         List<Product> products = service.findAll();
         model.addAttribute("products", products);
         return "productList";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editProduct(@PathVariable("id") String id, Model model) {
+        Product product = service.findById(id);
+        model.addAttribute("product", product);
+        return "editProduct";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String editProductPost(@PathVariable("id") String id, @ModelAttribute Product product, Model model) {
+        Product initialProduct = service.findById(id);
+        if (initialProduct == null || !product.getProductId().equals(initialProduct.getProductId())) {
+            return "redirect:list";
+        }
+        service.update(product);
+        return "redirect:list";
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable("id") String id, Model model) {
+        Product initialProduct = service.findById(id);
+        if (initialProduct == null) {
+            return ResponseEntity.notFound().build();
+        }
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -35,4 +35,19 @@ public class ProductServiceImpl implements ProductService {
         return allProducts;
     }
 
+    @Override
+    public Product findById(String id) {
+        return productRepository.findById(id);
+    }
+
+    @Override
+    public Product update(Product product) {
+        return productRepository.update(product);
+    }
+
+    @Override
+    public void delete(String id) {
+        productRepository.delete(id);
+    }
+
 }
