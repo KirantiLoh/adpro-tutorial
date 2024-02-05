54f147657279eac48c719c4fb41421c7c4be6855
Maurice Yang <mauriceyangmy28@gmail.com>
2024-02-05 20:35:55 +0700

    feat: Added edit feature

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -18,4 +18,20 @@ public class Product {
         return productId;
     }
 
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getProductQuantity() {
+        return productQuantity;
+    }
+
+    public void setProductQuantity(int productQuantity) {
+        this.productQuantity = productQuantity;
+    }
+
 }
