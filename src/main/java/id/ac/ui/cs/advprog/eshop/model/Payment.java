ae52cc4f4879b3248d585c9ed59dfe6f0403b88a
Maurice Yang <mauriceyangmy28@gmail.com>
2024-03-06 11:54:00 +0700

    [GREEN] Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -3,6 +3,7 @@ package id.ac.ui.cs.advprog.eshop.model;
 import java.util.Map;
 
 import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import lombok.Getter;
 
 @Getter
@@ -13,13 +14,24 @@ public class Payment {
     private Map<String, String> paymentData;
 
     public Payment(String id, String method, Map<String, String> paymentData) {
-
+        this.id = id;
+        this.method = method;
+        this.paymentData = paymentData;
+        this.status = "WAITING_CONFIRMATION";
     }
 
     public void setStatus(String status) {
+        if (!PaymentStatus.contains(status)) {
+            throw new IllegalArgumentException();
+        }
+        this.status = status;
     }
 
     public void setMethod(String method) {
+        if (!PaymentMethod.contains(method)) {
+            throw new IllegalArgumentException();
+        }
+        this.method = method;
     }
 
 }
--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -5,38 +5,46 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
 class PaymentTest {
+
+    private Map<String, String> paymentData = new HashMap<>();
+
     @Test
     void testCreateInvalidStatus() {
-        Payment payment = new Payment();
+        Payment payment = new Payment("1234567890", "VOUCHER", paymentData);
         assertThrows(IllegalArgumentException.class, () -> payment.setStatus("Invalid"));
     }
 
     @Test
     void testCreateValidStatus() {
-        Payment payment = new Payment();
+        Payment payment = new Payment("1234567890", "VOUCHER", paymentData);
         payment.setStatus("SUCCESS");
         assertEquals("SUCCESS", payment.getStatus());
     }
 
     @Test
     void testCreatePayment() {
-        Payment payment = new Payment("1234567890", "VOUCHER", "SUCCESS");
+        Payment payment = new Payment("1234567890", "VOUCHER", paymentData);
         assertEquals("1234567890", payment.getId());
         assertEquals("VOUCHER", payment.getMethod());
-        assertEquals("SUCCESS", payment.getStatus());
+        assertEquals(PaymentStatus.WAITING_PAYMENT.name(), payment.getStatus());
     }
 
     @Test
     void testSetMethod() {
-        Payment payment = new Payment();
+        Payment payment = new Payment("1234567890", "VOUCHER", paymentData);
         payment.setMethod("VOUCHER");
         assertEquals("VOUCHER", payment.getMethod());
     }
 
     @Test
     void testSetInvalidMethod() {
-        Payment payment = new Payment();
+        Payment payment = new Payment("1234567890", "VOUCHER", paymentData);
         assertThrows(IllegalArgumentException.class, () -> payment.setMethod("Invalid"));
     }
 }
