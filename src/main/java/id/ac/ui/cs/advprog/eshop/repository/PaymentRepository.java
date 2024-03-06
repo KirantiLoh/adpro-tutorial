bdf876ae9b4b5b1c0a70a42a7a73f06ce0dc2b84
Maurice Yang <mauriceyangmy28@gmail.com>
2024-03-06 21:08:57 +0700

    [GREEN] Implement PaymentRepository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -16,11 +16,42 @@ public class PaymentRepository {
     private List<Payment> payments = new ArrayList<>();
 
     public Payment addPayment(Order order, String method, Map<String, String> paymentData) {
-
-        return null;
+        Payment payment = new Payment(order.getId(), method, paymentData);
+        if (!PaymentMethod.contains(method)) {
+            throw new IllegalArgumentException("Invalid payment method");
+        }
+        if (paymentData.isEmpty()) {
+            throw new IllegalArgumentException("Payment data cannot be empty");
+        }
+        if (method.equals(PaymentMethod.VOUCHER.name())) {
+            int length = 0;
+            for (char c : paymentData.get("voucherCode").toCharArray()) {
+                if (Character.isDigit(c)) {
+                    length += 1;
+                }
+            }
+            if (paymentData.get("voucherCode") != null && (!paymentData.get("voucherCode").startsWith("ESHOP")
+                    || paymentData.get("voucherCode").length() != 16 || length != 8)) {
+                payment.setStatus(PaymentStatus.REJECTED.getValue());
+            }
+        } else if (method.equals(PaymentMethod.BANK_TRANSFER.name())) {
+            if (paymentData.get("bankName") == null || paymentData.get("bankName").isEmpty()) {
+                payment.setStatus(PaymentStatus.REJECTED.getValue());
+            }
+            if (paymentData.get("referenceCode") == null || paymentData.get("referenceCode").isEmpty()) {
+                payment.setStatus(PaymentStatus.REJECTED.getValue());
+            }
+        }
+        payments.add(payment);
+        return payment;
     }
 
     public Payment getPayment(String id) {
+        for (Payment payment : payments) {
+            if (payment.getId().equals(id)) {
+                return payment;
+            }
+        }
         return null;
     }
 
@@ -29,7 +60,12 @@ public class PaymentRepository {
     }
 
     public Payment updatePaymentStatus(String id, String status) {
-
+        for (Payment payment : payments) {
+            if (payment.getId().equals(id)) {
+                payment.setStatus(status);
+                return payment;
+            }
+        }
         return null;
     }
 }
